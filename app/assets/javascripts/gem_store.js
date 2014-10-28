b54864602605ac99e3960d4b3f4163f1cc68c6cc
Philip Nguyen <supertaru@gmail.com>
2014-10-27 20:50:35 -0700

    add pg

--- app/assets/javascripts/gem_store.js
+++ app/assets/javascripts/gem_store.js
@@ -1,4 +1,4 @@
 //=require_self
-//=require_tree ./gem_store_app
+//=require_tree ./gem_store
 var app = angular.module('GemStoreApp', ['store-directives']);
 var gemStoreDirectives = angular.module('store-directives', []);
