d7e8c7b0ff786cce6fadd21ec403d9654b174d34
Philip Nguyen <supertaru@gmail.com>
2014-11-01 20:11:49 -0700

    reviews, products, and categories CRUD connected to angular

--- app/assets/javascripts/gem_store.js
+++ app/assets/javascripts/gem_store.js
@@ -1,6 +1,8 @@
 //=require_self
 //=require_tree ./gem_store
-var app = angular.module('GemStoreApp', ['StoreDirectives']);
+
+
+var app = angular.module('GemStoreApp', ['StoreDirectives', 'CategoryDirectives']);
 // var gemStoreDirectives = angular.module('store-directives', []);
 app.config(['$httpProvider', function ($httpProvider) {
   $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
